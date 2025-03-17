42c54016ccaa5f4794dbc84e96cd859b86433ed4
Benjamin Davis <davis.benjamin41902@gmail.com>
2025-03-16 22:52:40 -0700

    first version of the router/query builder. very early, not done, does not work lol

--- src/lib/apex/sandbox.ts
+++ src/lib/apex/sandbox.ts
@@ -1,19 +1,22 @@
+import { Effect, Schema } from 'effect';
+import { mergeWithoutOverrides, type UnsetMarker } from './utils';
+
 interface ApexQueryDef {
 	input: unknown;
 	output: unknown;
 }
 
 type ApexRouterRecord = {
-	[key: string]: ApexQuery<any>;
+	[key: string]: AnyApexQuery;
 };
 
-type DecorateQuery<TQuery extends ApexQuery<any>> = (
-	input: TQuery['$types']['input']
-) => Promise<TQuery['$types']['output']>;
+type DecorateQuery<TQuery extends AnyApexQuery> = (
+	input: TQuery['_defs']['$types']['input']
+) => Promise<TQuery['_defs']['$types']['output']>;
 
 type DecorateRouterRecord<TRecord extends ApexRouterRecord> = {
 	[TKey in keyof TRecord]: TRecord[TKey] extends infer $Value
-		? $Value extends ApexQuery<any>
+		? $Value extends AnyApexQuery
 			? DecorateQuery<$Value>
 			: never
 		: never;
@@ -33,14 +36,18 @@ type ApexQueryOpts = {
 };
 
 export type ApexQuery<TDef extends ApexQueryDef> = {
-	_defs: {};
-	id: string;
-	$types: {
-		input: TDef['input'];
-		output: TDef['output'];
+	_defs: {
+		$types: {
+			input: TDef['input'];
+			output: TDef['output'];
+		};
+		id: string;
 	};
-	caller: (opts: ApexQueryOpts) => Promise<TDef['output']>;
+
+	(opts: ApexQueryOpts): Promise<TDef['output']>;
 };
+
+type AnyApexQuery = ApexQuery<any>;
 
 /**
  * Create an object without inheriting anything from `Object.prototype`
@@ -60,7 +67,7 @@ const createApexQueryCallerFactory = () => {
 
 export type DecorateCreateApexRouterOpts<TRouterOptions extends ApexRouterRecord> = {
 	[K in keyof TRouterOptions]: TRouterOptions[K] extends infer $Value
-		? $Value extends ApexQuery<any>
+		? $Value extends AnyApexQuery
 			? $Value
 			: never
 		: never;
@@ -92,3 +99,166 @@ export const createApexRouter = <TInput extends ApexRouterRecord>(
 
 	return router as ApexRouter<DecorateCreateApexRouterOpts<TInput>>;
 };
+
+const testSchema = Schema.Struct({
+	name: Schema.String,
+	age: Schema.Number
+});
+
+type InputType = Schema.Schema.Type<typeof testSchema>;
+
+const parseFnTest = async (input: string) => {
+	const result = await Effect.runPromise(
+		Effect.gen(function* () {
+			const schemaJson = Schema.parseJson(testSchema);
+			const decoded = yield* Schema.decode(schemaJson)(input);
+			return decoded;
+		})
+	);
+
+	return result;
+};
+
+export type ParseFn<TType> = (input: string) => Promise<TType>;
+
+function getParseFn<TType>(procedureParser: Schema.SchemaClass<TType, string>): ParseFn<TType> {
+	return async (input: string) => {
+		const result = await Effect.runPromise(
+			Effect.gen(function* () {
+				const decoded = yield* Schema.decode(procedureParser)(input);
+				return decoded;
+			})
+		);
+
+		return result;
+	};
+}
+
+type ParserWithOutput<TOutput> = Schema.SchemaClass<TOutput, string>;
+
+type Parser = ParserWithOutput<any>;
+
+type InferParser<TParser extends Parser> =
+	TParser extends ParserWithOutput<infer $TOut>
+		? {
+				out: $TOut;
+			}
+		: never;
+
+export type Simplify<TType> = TType extends any[] | Date ? TType : { [K in keyof TType]: TType[K] };
+
+type IntersectIfDefined<TType, TWith> = TType extends UnsetMarker
+	? TWith
+	: TWith extends UnsetMarker
+		? TType
+		: Simplify<TType & TWith>;
+
+type QueryResolverOpts<TInputOut> = {
+	input: TInputOut;
+};
+
+type QueryResolver<TInputOut, $Output> = (opts: QueryResolverOpts<TInputOut>) => $Output;
+
+type QueryBuilderResolver = (opts: QueryResolverOpts<any>) => any;
+
+export interface ApexQueryBuilder<TInputIn, TInputOut, TOutput> {
+	input<$Parser extends Parser>(
+		schema: $Parser
+	): ApexQueryBuilder<
+		IntersectIfDefined<TInputIn, string>,
+		IntersectIfDefined<TInputOut, InferParser<$Parser>['out']>,
+		TOutput
+	>;
+	query<$Output>(resolver: QueryResolver<TInputOut, $Output>): ApexQuery<{
+		input: TInputIn;
+		output: $Output;
+	}>;
+}
+
+type AnyApexQueryBuilder = ApexQueryBuilder<any, any, any>;
+
+type ApexQueryBuilderDef = {
+	parseFn?: ParseFn<any>;
+	resolver?: QueryBuilderResolver;
+};
+
+const createNewApexQueryBuilder = (data: {
+	def1: ApexQueryBuilderDef;
+	def2: Partial<ApexQueryBuilderDef>;
+}) => {
+	const { def1, def2 } = data;
+
+	return createApexQueryBuilder({
+		...mergeWithoutOverrides(def1, def2)
+	});
+};
+
+export const createApexQueryBuilder = (initDef: Partial<ApexQueryBuilderDef>) => {
+	const _def: ApexQueryBuilderDef = {
+		...initDef
+	};
+
+	const builder: AnyApexQueryBuilder = {
+		input(input) {
+			const parser = getParseFn(input);
+			return createNewApexQueryBuilder({
+				def1: _def,
+				def2: {
+					parseFn: parser
+				}
+			});
+		},
+		query(resolver) {
+			return createResolver({
+				resolver,
+				parseFn: _def.parseFn!
+			});
+		}
+	};
+
+	return builder;
+};
+
+const createResolver = (data: { resolver: QueryBuilderResolver; parseFn: ParseFn<any> }) => {
+	const { resolver, parseFn } = data;
+
+	const id = crypto.randomUUID();
+
+	const _def: AnyApexQuery['_defs'] = {
+		$types: null as any,
+		id
+	};
+
+	return createQueryCaller({
+		_def,
+		resolver,
+		parseFn
+	});
+};
+
+function createQueryCaller(data: {
+	_def: AnyApexQuery['_defs'];
+	resolver: QueryBuilderResolver;
+	parseFn: ParseFn<any>;
+}): AnyApexQuery {
+	const { _def, resolver, parseFn } = data;
+
+	async function procedure(opts: ApexQueryOpts) {
+		const parsed = await parseFn(opts.input as string);
+		const result = resolver({
+			input: parsed
+		});
+
+		return result;
+	}
+
+	procedure._defs = _def;
+
+	return procedure;
+}
+
+const builder = createApexQueryBuilder({});
+
+const query = builder.input(Schema.parseJson(testSchema));
+
+// TODO: keep working on getting the query builder to work. Start by testing console logging the current states of the builder
--- src/lib/apex/utils.ts
+++ src/lib/apex/utils.ts
@@ -1,3 +1,24 @@
 /** @internal */
 export const unsetMarker = Symbol();
 export type UnsetMarker = typeof unsetMarker;
+
+/**
+ * Ensures there are no duplicate keys when building a procedure.
+ * @internal
+ */
+export function mergeWithoutOverrides<TType extends Record<string, unknown>>(
+	obj1: TType,
+	...objs: Partial<TType>[]
+): TType {
+	const newObj: TType = Object.assign(Object.create(null), obj1);
+
+	for (const overrides of objs) {
+		for (const key in overrides) {
+			if (key in newObj && newObj[key] !== overrides[key]) {
+				throw new Error(`Duplicate key ${key}`);
+			}
+			newObj[key as keyof TType] = overrides[key] as TType[keyof TType];
+		}
+	}
+	return newObj;
+}
